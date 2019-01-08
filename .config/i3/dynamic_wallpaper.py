b7fecdd23779cec1860418b89a9670cf8dd5cf46
Yusuf Mohammad <yousufinternet@gmail.com>
2019-01-08 21:12:19 +0300

    Failed attempts to use pywal due to a bug

--- .config/i3/dynamic_wallpaper.py
+++ .config/i3/dynamic_wallpaper.py
@@ -5,6 +5,7 @@ import time
 import os
 import subprocess
 import inspect
+import pipes
 
 script_path = os.path.dirname(
     os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -13,5 +14,10 @@ while True:
     wall_no = round((current_hr/24) * 15) + 1
     wall_path = os.path.join(
         script_path, 'mojave_dynamic', 'mojave_dynamic_%s.jpeg' % wall_no)
+    print(wall_path)
+    shell_esc_path = pipes.quote(wall_path)
     subprocess.Popen(['feh', '--bg-scale', wall_path])
-    time.sleep(60)
+    # subprocess.Popen('wal -b colorz -i %s' % shell_esc_path, shell=True)
+    wal_oomox_colors = pipes.quote(os.path.expanduser('~/.cache/wal/colors-oomox'))
+    # subprocess.Popen('oomox-materia-cli -o wal -d true %s' % wal_oomox_colors, shell=True)
+    time.sleep(400)
