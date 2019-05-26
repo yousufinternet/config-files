ddda05bc8fa6a5e0f68086a09e0fb49ace2b3608
Yusuf Mohammad <yousufinternet@gmail.com>
2019-05-26 07:47:55 +0300

    compton options

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-compton -f &
+compton -f --inactive-dim 0.1 -c --backend glx &
 mpd &
 systemctl --user start xfce4-notifyd &
 exec ~/.config/i3/dynamic_wallpaper.py &
@@ -13,4 +13,5 @@ pasystray &
 syncthing-gtk -m &
 parcellite &
 exec ~/Documents/sshfs-watchdog/sshfs-wd.sh &
-localectl --no-convert set-x11-keymap us,ara pc104 ,qwerty 'grp:alt_shift_toggle,lv3:menu_switch' &
+localectl --no-convert set-x11-keymap us,ara pc104 ,qwerty 'grp:alt_shift_toggle,caps:swapescape' &
+setxkbmap -option caps:escape &
