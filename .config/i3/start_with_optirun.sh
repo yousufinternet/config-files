21983dab78ecac602b306d7fee3ef87418bd1fa6
Yusuf Mohammad <yousufinternet@gmail.com>
2019-08-20 00:03:06 +0300

    start with optirun script

---
+++ .config/i3/start_with_optirun.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# if [ $GDK_SCALE = 2 ]; then
+#     hybrid="optirun"
+# else
+#     hybrid=""
+# fi
+
+# bumblebee_installed=$(pacman -Qe bumblebee| wc -l)
+# if [ $bumblebee_installed = 1 ]; then
+# '-n' means the string is not of zero length
+if [ -n $(pacman -Qe bumblebee| wc -l) ]; then
+    hybrid="optirun"
+fi
+
+echo $hybrid $@
+exec $hybrid $@ &
+
+exit
