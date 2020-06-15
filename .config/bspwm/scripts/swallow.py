13e5c28727969ac18f067a655c6cf7e4f41c3685
Yusuf Mohammad <yousufinternet@gmail.com>
2020-06-15 20:37:26 +0300

    Logging and more exception handling in swallow script

--- .config/bspwm/scripts/swallow.py
+++ .config/bspwm/scripts/swallow.py
@@ -2,21 +2,30 @@
 '''
 Swallow function for bspwm
 when spawning a child process hide the parent window
+WARNING: not tested on multiple monitors setup
 '''
 
+import os
 import re
+import sys
 import json
+import logging
+import traceback
 import subprocess
 from functools import partial
 
 
-EXCLUDED_CLASSES = ['qutebrowser', 'xev']
+EXCLUDED_CLASSES = ['qutebrowser', 'xev', 'lemonbar', 'xfce4-panel']
+
+logging_level = logging.ERROR if len(sys.argv) == 1 else sys.argv[1]
+logging.basicConfig(filename=os.path.expanduser('~/.swallow.log'),
+                    level=logging_level)
 
 cmd_run = partial(subprocess.Popen, text=True, shell=True)
 
 
 def get_class(wid):
-    '''
+    '''lemonbar
     get passed window id class using xprop output
     '''
     try:
@@ -24,8 +33,10 @@ def get_class(wid):
         wids = re.search(r'^wm_class\(string\)\s=\s(.*?)$', out,
                          flags=re.IGNORECASE | re.MULTILINE).group(1)
         wids = [wid.strip('"') for wid in wids.split(', ')][1]
+        logging.debug(f'get_class output: {wids} {out} {wid}')
         return wids
-    except Exception:
+    except Exception as e:
+        logging.debug(f'Error in get_class: {wid}: {e}')
         return ''
 
 
@@ -54,15 +65,11 @@ def execute(cmd):
         raise subprocess.CalledProcessError(return_code, cmd)
 
 
-def desk_layout(d_id, m_id):
+def desk_layout():
     '''
     return True if desktop is monocle
     '''
-    desk_idx = cmd_output('bspc query -D --names').split('\n').index(d_id)
-    mon_idx = cmd_output('bspc query -M --names').split('\n').index(m_id)
-    state = json.loads(cmd_output('bspc wm -d'))
-    desktop_layout = state['monitors'][mon_idx]['desktops'][desk_idx]['layout']
-    return desktop_layout
+    return json.loads(cmd_output('bspc query -T -d'))['layout']
 
 
 def is_fullscreen(wid):
@@ -106,9 +113,9 @@ def swallow_cond(new_wid, last_wid):
     True if window should be swallowed False if not
     Modify it to your liking
     '''
-    current_desk = cmd_output('bspc query -D -d --names')
-    current_mon = cmd_output('bspc query -M -m --names')
-    if desk_layout(current_desk, current_mon) == 'monocle':
+    logging.debug(f'swallow_cond: {new_wid} {last_wid}')
+    if desk_layout() == 'monocle':
+        logging.debug('current desktop is monocle returning False')
         return False
     is_excluded = [c for c in EXCLUDED_CLASSES
                    if any(c in get_class(id)
@@ -129,29 +136,41 @@ def swallow():
     '''
     swallowed = {}
     for event in execute('bspc subscribe node_add node_remove'):
-        event = event.split()
-        if event[0] == 'node_add':
-            new_wid = event[-1]
-            last_wid = cmd_output('bspc query -N -d -n last.window')
-            if not swallow_cond(new_wid, last_wid):
+        try:
+            logging.debug(f'Processing Event: {event}')
+            event = event.split()
+            if not event:
                 continue
-            new_pid = get_pid(new_wid)
-            last_pid = get_pid(last_wid)
-            if not all([new_pid, last_pid]):
-                continue
-            if is_child(last_pid, new_pid):
-                cmd_run(f'bspc node {last_wid} --flag private=on')
-                cmd_run(f'bspc node --swap {last_wid} --follow')
-                cmd_run(f'bspc node {last_wid} --flag hidden=on')
-                cmd_run(f'bspc node {new_wid} --flag private=on')
-                swallowed[new_wid] = last_wid
-        if event[0] == 'node_remove':
-            removed_wid = event[-1]
-            if removed_wid in swallowed.keys():
-                swallowed_id = swallowed[removed_wid]
-                cmd_run(f'bspc node {swallowed_id} --flag hidden=off')
-                cmd_run(f'bspc node --focus {swallowed_id}')
+            if event[0] == 'node_add':
+                new_wid = event[-1]
+                last_wid = cmd_output('bspc query -N -d -n last.window')
+                if not swallow_cond(new_wid, last_wid):
+                    continue
+                new_pid = get_pid(new_wid)
+                last_pid = get_pid(last_wid)
+                if not all([new_pid, last_pid]):
+                    continue
+                if is_child(last_pid, new_pid):
+                    cmd_run(f'bspc node {last_wid} --flag private=on')
+                    cmd_run(f'bspc node --swap {last_wid} --follow')
+                    cmd_run(f'bspc node {last_wid} --flag hidden=on')
+                    cmd_run(f'bspc node {new_wid} --flag private=on')
+                    swallowed[new_wid] = last_wid
+            if event[0] == 'node_remove':
+                removed_wid = event[-1]
+                if removed_wid in swallowed.keys():
+                    swallowed_id = swallowed[removed_wid]
+                    cmd_run(f'bspc node {swallowed_id} --flag hidden=off')
+                    cmd_run(f'bspc node --focus {swallowed_id}')
+        except Exception as e:
+            logging.debug('Error occured in mainloop:'
+                          f'\n{e}\n{traceback.format_exc()}')
 
 
 if __name__ == '__main__':
-    swallow()
+    try:
+        swallow()
+    except Exception as e:
+        logging.debug('Error occured in mainloop:'
+                      f'\n{e}\n{traceback.format_exc()}')
+
