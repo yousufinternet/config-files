3caeb50abc0e1dd4d7d93d604641e13261d79d3e
Yusuf Mohammad <yousufinternet@gmail.com>
2020-09-24 10:58:20 +0300

    herbstluftwm autostart file in python

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
-compton -f &
-mpd &
-systemctl --user start xfce4-notifyd &
-safeeyes &
+localectl --no-convert set-x11-keymap us,ara pc104 ,qwerty 'grp:alt_shift_toggle,caps:swapescape' &
+setxkbmap -option "caps:escape" -option "altwin:menu_win" -option "terminate:ctrl_alt_bksp" &
+picom &
+~/.config/bspwm/scripts/set_wallpaper.py &
 redshift &
-goldendict &
+sxhkd -c ~/.config/sxhkd/apps_keys &
+systemctl --user start xfce4-notifyd &
+~/.config/bspwm/bar/start_bar_i3.py &
+xfce4-panel -d &
+mpd &
 nm-applet &
 udiskie -t &
 blueman-applet &
 pasystray &
-syncthing-gtk -m &
 parcellite &
 emacs --daemon &
-kdeconnect-indicator &
-~/.config/i3/dynamic_wallpaper.py &
-XDG_CONFIG_HOME=~/.config_alt xfce4-panel -d &
 # exec ~/Documents/sshfs-watchdog/sshfs-wd.sh &
-localectl --no-convert set-x11-keymap us,ara pc104 ,qwerty 'grp:alt_shift_toggle,caps:swapescape' &
-setxkbmap -option caps:escape &
+perWindowLayoutD &
