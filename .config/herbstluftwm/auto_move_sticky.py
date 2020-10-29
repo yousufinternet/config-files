32287668b87a82f3acd885bf1a785705f71b91d2
Yusuf Mohammad <yousufinternet@gmail.com>
2020-10-29 16:07:36 +0300

    some updates to sticky scripts in herbstluftwm

---
+++ .config/herbstluftwm/auto_move_sticky.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import subprocess as sp
+
+
+def execute(cmd):
+    popen = sp.Popen(cmd, stdout=sp.PIPE,
+                     universal_newlines=True, text=True, shell=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise sp.CalledProcessError(return_code, cmd)
+
+
+for line in execute('herbstclient --idle tag_changed'):
+    sp.Popen(os.path.expanduser('~/.config/herbstluftwm/switch_tag.py'),
+             shell=True)
---
+++ .config/herbstluftwm/monitor_sticky_rules.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import subprocess
+
+
+def hc(*args):
+    P = subprocess.Popen(
+        f'herbstclient {" ".join(args)}', text=True, shell=True,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    err = not bool(P.stderr.read())
+    return P.stdout.read(), err
+
+
+def execute(cmd):
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                             universal_newlines=True, text=True, shell=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+
+
+for event in execute('herbstclient --idle'):
+    event = event.strip().split()
+    # make sticky windows sticky!
+    if event[0] == 'rule' and event[1] == 'make_sticky':
+        hc(f'chain .-. new_attr bool clients.{event[2]}.my_sticky'
+           f' .-. set_attr clients.{event[2]}.my_sticky on')
+        
