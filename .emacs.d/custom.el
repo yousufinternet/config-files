7b989e6d093edc695fd3a7f6087bc961095929c4
Yusuf Mohammad <yousufinternet@gmail.com>
2020-10-30 22:38:37 +0300

    removed custom org-agenda-files variable

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -13,7 +13,6 @@
  '(jdee-db-requested-breakpoint-face-colors (cons "#1b1d1e" "#60aa00"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#1b1d1e" "#505050"))
  '(objed-cursor-color "#d02b61")
- '(org-agenda-files '("~/Public/Syncthing Global/Notes/TODO.org"))
  '(package-selected-packages
    '(org-protocol yasnippet-snippets rainbow-mode rainbow-delimiters spacemacs-theme counsel org-bullets evil-leader zerodark-theme gruvbox-theme modus-vivendi-theme lsp-ui lsp-mode evil-collection evil-org evil-easymotion evil-iedit evil-iedit-state use-package))
  '(pdf-view-midnight-colors (cons "#dddddd" "#1b1d1e"))
