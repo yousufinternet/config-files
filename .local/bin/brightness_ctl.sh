a614a37204d8d550ff25ddc2b76a07f15068ea83
Yusuf Mohammad <yousufinternet@gmail.com>
2020-11-09 00:09:30 +0300

    dunst instead of xfce4-notifyd

--- .local/bin/brightness_ctl.sh
+++ .local/bin/brightness_ctl.sh
@@ -6,13 +6,14 @@ light -$light_par $amount
 
 cur_bright=$(light | grep -o -E '[[:digit:]]+' | head -n 1)
 
-icon='display-brightness-off'
+icon='notification-display-brightness-off'
 if [ $cur_bright -gt 70 ]; then
-    icon='display-brightness-high'
+    icon='notification-display-brightness-high'
 elif [ $cur_bright -gt 40 ]; then
-    icon='display-brightness-medium'
+    icon='notification-display-brightness-medium'
 elif [ $cur_bright -gt 0 ]; then
-    icon='display-brightness-low'
+    icon='notification-display-brightness-low'
 fi
 
-notify-send.py brightness --replaces-process brightness_ctl -i $icon --hint int:value:$cur_bright
+#notify-send.py brightness --replaces-process brightness_ctl -i $icon --hint int:value:$cur_bright
+dunstify --replace=002 --icon=$icon --hints=int:value:$cur_bright --appname=Brightness ""
--- .local/bin/volume_ctl.sh
+++ .local/bin/volume_ctl.sh
@@ -4,13 +4,14 @@ pactl set-sink-volume @DEFAULT_SINK@ $@%
 
 cur_vol=$(pamixer --get-volume)
 
-icon='audio-volume-muted'
+icon='notification-audio-volume-muted'
 if [ $cur_vol -gt 70 ]; then
-    icon='audio-volume-high'
+    icon='notification-audio-volume-high'
 elif [ $cur_vol -gt 40 ]; then
-    icon='audio-volume-medium'
+    icon='notification-audio-volume-medium'
 elif [ $cur_vol -gt 0 ]; then
-    icon='audio-volume-low'
+    icon='notification-audio-volume-low'
 fi
 
-notify-send.py volume --replaces-process volume_ctl -i $icon --hint int:value:$cur_vol
+#notify-send.py volume --replaces-process volume_ctl -i $icon --hint int:value:$cur_vol
+dunstify --replace=001 --icon=$icon --hints=int:value:$cur_vol --appname=Volume ""
