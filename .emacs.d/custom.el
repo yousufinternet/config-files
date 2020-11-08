d05c8a6590120b2a62039fdeac3552fa6d487ed8
Yusuf Mohammad <yousufinternet@gmail.com>
2020-11-09 00:07:06 +0300

    org packages

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -14,7 +14,7 @@
  '(jdee-db-spec-breakpoint-face-colors (cons "#1b1d1e" "#505050"))
  '(objed-cursor-color "#d02b61")
  '(package-selected-packages
-   '(org-protocol yasnippet-snippets rainbow-mode rainbow-delimiters spacemacs-theme counsel org-bullets evil-leader zerodark-theme gruvbox-theme modus-vivendi-theme lsp-ui lsp-mode evil-collection evil-org evil-easymotion evil-iedit evil-iedit-state use-package))
+   '(yasnippet-snippets rainbow-mode rainbow-delimiters spacemacs-theme counsel org-bullets evil-leader zerodark-theme gruvbox-theme modus-vivendi-theme lsp-ui lsp-mode evil-collection evil-org evil-easymotion evil-iedit evil-iedit-state use-package))
  '(pdf-view-midnight-colors (cons "#dddddd" "#1b1d1e"))
  '(rustic-ansi-faces
    ["#1b1d1e" "#d02b61" "#60aa00" "#d08928" "#6c9ef8" "#b77fdb" "#00aa80" "#dddddd"])
