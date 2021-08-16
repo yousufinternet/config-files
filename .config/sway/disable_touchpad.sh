89b49c59b82ddbe66f14046e341c8aac1458b01b
Yusuf Mohammad <yousufinternet@gmail.com>
2021-08-16 12:05:20 +0300

    more sway bindings

--- .config/sway/disable_touchpad.sh
+++ .config/sway/disable_touchpad.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ $HOSTNAME='yusufs-lenovo' ]
+if [ $HOSTNAME = 'yusufs-lenovo' ]
 then
     swaymsg input "2:7:SynPS/2_Synaptics_TouchPad" events disabled
     swaymsg input "1:1:AT_Translated_Set_2_keyboard" events disabled
