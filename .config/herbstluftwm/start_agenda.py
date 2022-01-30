dfc247d751e0e973f0f857a69af9b0d9d6a75e50
Yusuf Mohammad <yousufinternet@gmail.com>
2022-01-30 16:13:06 +0300

    script for starting emacsclient window with agenda on agenda tag

---
+++ .config/herbstluftwm/start_agenda.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import subprocess as sp
+from auto_move_sticky import execute
+
+LISP_ARGS = ['(setq frame-title-format "EmacsAgenda")', '(org-agenda nil "a")',
+             '(delete-other-windows)', '(setq mode-line-format nil)']
+LISP_ARGS = '-e '+'-e '.join(f"'{a}'" for a in LISP_ARGS)
+AGENDA_CMD = f"emacsclient -c --no-wait -q -a '' {LISP_ARGS}"
+
+print(AGENDA_CMD)
+sp.Popen(AGENDA_CMD, shell=True, text=True)
+
+for line in execute('herbstclient -c 10 --idle window_title_changed'):
+    line = [i.strip() for i in line.split()]
+    if line[0] == 'window_title_changed' and line[-1] == 'EmacsAgenda':
+        sp.Popen(f'herbstclient apply_tmp_rule {line[1]} title=EmacsAgenda tag=AGENDA', shell=True, text=True)
+        break
+    
