3dc58bdcbb32b71d493b0ef6c0fec69bf561c71a
Yusuf Mohammad <yousufinternet@gmail.com>
2022-02-21 06:27:06 +0300

    updated script with WOL command

--- Scripts/push_movie_tv.py
+++ Scripts/push_movie_tv.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 
 import sys
+import time
 import subprocess as sp
 from functools import partial
 
 cmdout = partial(sp.check_output, shell=True, text=True)
 cmdrun = partial(sp.Popen, shell=True, text=True)
 
+
 devices = cmdout('adb devices')
 if not '192.168.1.3' in devices:
-    p = cmdrun('adb connect 192.168.1.3')
-    p.wait()
+    try:
+        p = cmdrun('adb connect 192.168.1.3')
+        p.wait()
+    except sp.CalledProcessError:
+        p = cmdrun('wol CC:A1:2B:B9:45:8A')
+        p.wait()
+        time.sleep(2)
+        p = cmdrun('adb connect 192.168.1.3')
+        p.wait()
+
 
 storages = cmdout('adb -s 192.168.1.3 shell ls storage')
 if not 'F65E1BDD5E1B9609' in storages:
