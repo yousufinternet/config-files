e47910b3a362977b2b36c5849a6e1c10978eed3c
Yusuf Mohammad <yousufinternet@gmail.com>
2024-01-26 12:10:21 +0300

    Use xbacklight (from acpi_backlight package) as light is not maintained, only use ddccontrol if external monitor is present

--- .local/bin/toggle_brightness.py
+++ .local/bin/toggle_brightness.py
@@ -12,24 +12,29 @@ def main(brightness):
 
 def gradual_inrease(brightness):
     if not brightness:
-        brightness = subprocess.check_output(['light'])
-        brightness = int(float(brightness[:-2]))
+        brightness = subprocess.check_output(['xbacklight', '-getf'])
+        brightness = int(float(brightness.strip()))
+    monitors = subprocess.check_output('xrandr', text=True)
+    monitors = len([l for l in monitors.splitlines()[1:] if l[0] != ' ' and l.split()[1] == 'connected'])
+
     if brightness >= 50 :
         for i in range(brightness, 2,-1):
             time.sleep(0.02)
-            subprocess.Popen(['light', '-S', str(i)])
-        try:
-            subprocess.Popen('sudo ddccontrol -r 0x10 -w 0 dev:/dev/i2c-4'.split())
-        except:
-            pass
+            subprocess.Popen(['xbacklight', str(i)])
+        if monitors > 1:
+            try:
+                subprocess.Popen('sudo ddccontrol -r 0x10 -w 0 dev:/dev/i2c-4'.split())
+            except:
+                pass
     else:
         for i in range(brightness, 101,1):
             time.sleep(0.02)
-            subprocess.Popen(['light', '-S', str(i)])
-        try:
-            subprocess.Popen('sudo ddccontrol -r 0x10 -w 75 dev:/dev/i2c-4'.split())
-        except:
-            pass
+            subprocess.Popen(['xbacklight', str(i)])
+        if monitors > 1:
+            try:
+                subprocess.Popen('sudo ddccontrol -r 0x10 -w 75 dev:/dev/i2c-4'.split())
+            except:
+                pass
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
