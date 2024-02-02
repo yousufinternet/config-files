3e9e3204e1dc220b79688eff15a1f97ffe360bbf
Yusuf Mohammad <yousufinternet@gmail.com>
2024-02-02 18:43:34 +0300

    dunst notifications control from eww bar

---
+++ .config/eww/scripts/dunst_ctl.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+import subprocess as sp
+
+paused = '\uf4b3'
+notify = '\uf075'
+
+notifications_monitor = sp.Popen('dbus-monitor "interface=\'org.freedesktop.Notifications\'" | grep --line-buffered "member=Notify"', stdout=sp.PIPE, text=True, shell=True)
+
+
+def output_wrapper():
+    is_paused = sp.check_output(['dunstctl', 'is-paused'], text=True).strip() == 'true'
+    waiting = sp.check_output(['dunstctl', 'count'], text=True).strip().splitlines()[0].split(':')[1].strip()
+    waiting = waiting if int(waiting) > 0 else ""
+
+    icon_color = "grey-icon" if is_paused else "blue-icon"
+    icon = f'(label :class "{icon_color}" :text "{paused if is_paused else notify}")'
+
+    if waiting:
+        icon = f'(box :spacing 2 {icon} "{waiting}")'
+    print(f'(eventbox :onclick `~/.config/eww/scripts/dunsttoggle.sh` :onrightclick `dunstctl history-pop` {icon})')
+
+while True:
+    output_wrapper()
+    # print("(label :text 'test')")
+    sys.stdout.flush()
+    notifications_monitor.stdout.readline()
+
---
+++ .config/eww/scripts/dunsttoggle.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+dunstctl set-paused toggle;
+notify-send -a dunst "Notifications visibility were toggled"
