331ac73ee288e6f38dde121a769059f5e443082c
Yusuf Mohammad <yousufinternet@gmail.com>
2024-02-03 22:14:43 +0300

    Updated herbstluftwm theming and remaining files from eww update

--- Scripts/raise_xfce4_panel.py
+++ Scripts/raise_xfce4_panel.py
@@ -3,21 +3,21 @@
 import time
 import subprocess as sp
 
+def get_class_ids(class_name):
+    try:
+        eww_ids = sp.check_output(f'xdo id -N "{class_name}"', text=True, shell=True).splitlines()
+        return eww_ids
+    except sp.CalledProcessError:
+        return []
+
 time.sleep(1)
+class_names = ('Eww', 'eww-top_bar', 'eww-top_bar_2', 'eww-v_bar_2', 'eww-vertical_bar')
 xfce_ids = sp.check_output(
     'xdo id -a xfce4-panel', text=True, shell=True).strip().splitlines()
-try:
-    eww_ids = sp.check_output('xdo id -N "Eww"', text=True, shell=True).splitlines()
-except sp.CalledProcessError:
-    pass
-try:
-    eww_ids = sp.check_output('xdo id -N "eww-top_bar"', text=True, shell=True).splitlines()
-except sp.CalledProcessError:
-    pass
-try:
-    eww_ids = sp.check_output('xdo id -N "eww-top_bar_2"', text=True, shell=True).splitlines()
-except sp.CalledProcessError:
-    pass
+eww_ids = []
+for cls_name in class_names:
+    eww_ids += get_class_ids(cls_name)
+
 for i in xfce_ids:
     for e in eww_ids:
         sp.Popen(
