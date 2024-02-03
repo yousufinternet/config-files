14c6930df2cec6b29066e53e7bbc0b1444677651
Yusuf Mohammad <yousufinternet@gmail.com>
2024-02-03 16:53:34 +0300

    Better theming for herbstluftwm and do not focus sticky windows when switching tags

--- .config/herbstluftwm/switch_tag.py
+++ .config/herbstluftwm/switch_tag.py
@@ -15,20 +15,30 @@ def hc(*args):
 
 hc('lock')
 
+# The try block so that we ensure herbtluftwm always gets unlocked
 try:
     clients = [wid.rstrip('.') for wid in
             hc('attr clients')[0].split() if wid.startswith('0x')]
 
+    focused_client = hc('attr clients.focus.winid')
+    focused_client = None if not focused_client[1] else focused_client[0].strip()
     sticky_clts = [
         wid for wid in clients
         if hc(f'get_attr clients.{wid}.my_sticky')[0].rstrip() == 'true'
     ]
 
+    tiling_or_fullscreen = False
     for wid in sticky_clts:
-        was_min = eval(hc(f'attr clients.{wid}.minimized')[0].title())
+        was_min = hc(f'attr clients.{wid}.minimized')[0].strip() == 'true'
+        fullscreen = hc(f'attr clients.{wid}.fullscreen')[0].strip() == 'true'
+        tiling = hc(f'attr clients.{wid}.floating')[0].strip() == 'false'
+        tiling_or_fullscreen = any((tiling_or_fullscreen, fullscreen, tiling))
         hc(f'bring {wid}')
         if was_min:
             hc(f'set_attr clients.{wid}.minimized on')
+    # switch to the originally focused window unless the sticky window is tiling or fullscreen
+    if focused_client is not None and not tiling_or_fullscreen:
+        hc(f'jumpto {focused_client}')
 except Exception:
     pass
 
